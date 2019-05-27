4413794cd17cdc88fae0e3baaa8b8e08024683f5
realsst1 <shreyesh2008@gmail.com>
2019-05-27 13:39:32 +0530

    made basic ui of chat activity

--- app/src/main/java/com/example/shreyesh/ChatFragment.java
+++ app/src/main/java/com/example/shreyesh/ChatFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,18 @@ import android.view.ViewGroup;
 
 public class ChatFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private MessageAdapter adapter;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        System.out.println("Chat Fragment Loaded");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chat, container, false);
+
     }
 
 }
