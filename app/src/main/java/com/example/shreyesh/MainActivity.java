1a45c5c3313a4d6846fe4befef555ce242f6c25e
realsst1 <shreyesh2008@gmail.com>
2019-05-27 13:13:31 +0530

    Added fragments

--- app/src/main/java/com/example/shreyesh/MainActivity.java
+++ app/src/main/java/com/example/shreyesh/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.shreyesh;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction transaction=fragmentManager.beginTransaction();
+        transaction.add(R.id.frag,new ChatFragment());
+
+        transaction.commit();
     }
 }
