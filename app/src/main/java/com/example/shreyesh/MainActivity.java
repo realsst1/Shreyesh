dce1e7b1098486ce049b414aa3f2cc67324b3a77
realsst1 <s@Nuchotu1>
2019-05-29 12:22:46 +0530

    net connectivity check added

--- app/src/main/java/com/example/shreyesh/MainActivity.java
+++ app/src/main/java/com/example/shreyesh/MainActivity.java
@@ -1,21 +1,81 @@
 package com.example.shreyesh;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ChatFragment chatFragment;
+    private ImageView send,sendImage;
+    private EditText message;
+    private Toolbar mainToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        send=(ImageView)findViewById(R.id.sendMessage);
+        sendImage=(ImageView)findViewById(R.id.addImage);
+        message=(EditText)findViewById(R.id.messageToSend);
+
+        mainToolbar=(Toolbar)findViewById(R.id.mainToolbar);
+        setSupportActionBar(mainToolbar);
+
+        chatFragment=new ChatFragment();
+
         FragmentManager fragmentManager=getSupportFragmentManager();
         FragmentTransaction transaction=fragmentManager.beginTransaction();
-        transaction.add(R.id.frag,new ChatFragment());
-
+        transaction.add(R.id.frag_container,chatFragment);
         transaction.commit();
+
+        this.registerReceiver(this.mConnReceiver,
+                new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+
+
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Clicked");
+            }
+        });
+
     }
+
+    private BroadcastReceiver mConnReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            boolean noConnectivity = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+            String reason = intent.getStringExtra(ConnectivityManager.EXTRA_REASON);
+            boolean isFailover = intent.getBooleanExtra(ConnectivityManager.EXTRA_IS_FAILOVER, false);
+
+            NetworkInfo currentNetworkInfo = (NetworkInfo) intent.getParcelableExtra(ConnectivityManager.EXTRA_NETWORK_INFO);
+            NetworkInfo otherNetworkInfo = (NetworkInfo) intent.getParcelableExtra(ConnectivityManager.EXTRA_OTHER_NETWORK_INFO);
+
+            if(currentNetworkInfo.isConnected()){
+                //Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_LONG).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frag_container,new ChatFragment()).commitAllowingStateLoss();
+                send.setEnabled(true);
+                sendImage.setEnabled(true);
+            }else{
+                //Toast.makeText(getApplicationContext(), "Not Connected", Toast.LENGTH_LONG).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frag_container,new NoInternetFragment()).commitAllowingStateLoss();
+                send.setEnabled(false);
+                sendImage.setEnabled(false);
+            }
+        }
+    };
 }
--- app/src/main/java/com/example/shreyesh/Message.java
+++ app/src/main/java/com/example/shreyesh/Message.java
@@ -1,4 +1,52 @@
 package com.example.shreyesh;
 
 public class Message {
+
+    private String type;
+    private String from;
+    private String text;
+    private String timestamp;
+
+    public Message(String type, String from, String text, String timestamp) {
+        this.type = type;
+        this.from = from;
+        this.text = text;
+        this.timestamp = timestamp;
+    }
+
+
+    public Message() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 }
